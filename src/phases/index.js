b1454c2fbd63bf25f8a19aec828820c1f97a55b0
mjeffery <michael.jeffery@gmail.com>
2016-03-15 20:38:57 -0400

    some more phases

--- src/phases/index.js
+++ src/phases/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 let phases = {
-	'game-start': require('./GameStartPhase')
+	'session-start': require('./SessionStart'),
+	'waiting-for-players': require('./WaitingForPlayers'),
+	'game-start': require('./GameStart')
 };
 
 module.exports = function findPhase(id) { return phases[id]; }
