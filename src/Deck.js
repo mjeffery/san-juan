e75f62d7ecdd3901e29450db5a3284e475cca89b
mjeffery <michael.jeffery@gmail.com>
2016-03-20 21:58:49 -0400

    some simple fixes to the Deck class

--- src/Deck.js
+++ src/Deck.js
@@ -8,19 +8,28 @@ class Deck {
     }
 
     draw(){
-        return _cards.pop();
+		if(this._cards.length === 0) {
+			this.reset();
+		}
+
+		return this._cards.pop();
     }
+
+	discard(card) {
+		this._discard.push(card);
+	}
 
     reset(){
         this._cards = shuffle(Array.concat(this._cards, this._discard));
+		this._discard = [];
     }
 
     size(){
-        return this._cards.length
+        return this._cards.length;
     }
 
     discardSize(){
-        return this._discard.length
+        return this._discard.length;
     }
 
     serializePublic(){
